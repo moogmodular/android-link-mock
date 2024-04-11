141512f999d95616a9c8e6ab291d93d607fa8c83
moogmodular <mmodular@protonmail.com>
2024-04-11 09:34:28 +0200

    feat: app link mock

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log(Bun.env)
+console.log("Server running!")
+
+import {readFile} from 'fs/promises';
+import {join} from 'path';
+
+
+Bun.serve({
+    async fetch(req) {
+        const url = new URL(req.url);
+        console.log(`Request for ${url}`);
+        if (url.pathname === '/.well-known/assetLinks.json') {
+            const filePath = join(process.cwd(), 'public/.well-known', 'assetLinks.json');
+            try {
+                const fileContent = await readFile(filePath, 'utf-8');
+
+                return new Response(fileContent, {
+                    headers: { 'Content-Type': 'application/json' },
+                });
+            } catch (error) {
+                return new Response('Internal Server Error', { status: 500 });
+            }
+        } else {
+            return new Response('Not Found', { status: 404 });
+        }
+    },
+});
