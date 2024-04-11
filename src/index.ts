79fcd379064e645a3e2be814520a910c7e26ff2d
moogmodular <mmodular@protonmail.com>
2024-04-11 09:53:58 +0200

    feat: case

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ Bun.serve({
     async fetch(req) {
         const url = new URL(req.url);
         console.log(`Request for ${url}`);
-        if (url.pathname === '/.well-known/assetLinks.json') {
-            const filePath = join(process.cwd(), 'public/.well-known', 'assetLinks.json');
+        if (url.pathname === '/.well-known/assetlinks.json') {
+            const filePath = join(process.cwd(), 'public/.well-known', 'assetlinks.json');
             try {
                 const fileContent = await readFile(filePath, 'utf-8');
 
